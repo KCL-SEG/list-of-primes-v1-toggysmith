b156ae3ea0cbc51b2d2bcc47ba4657a2dacd0877
Toggy Smith <tony.o.smith@kcl.ac.uk>
2022-09-30 13:15:31 +0100

    Generate an n-sized list of positive prime numbers

--- primes.py
+++ primes.py
@@ -3,4 +3,22 @@
 
 def primes(number_of_primes):
     list = []
+
+    primesRemaining = number_of_primes
+    currentNumber = 2
+
+    while primesRemaining != 0:
+        isCurrentNumberPrime = True
+
+        for n in range(2, currentNumber - 1):
+            if currentNumber % n == 0:
+                isCurrentNumberPrime = False
+                break
+
+        if isCurrentNumberPrime:
+            primesRemaining -= 1
+            list.append(currentNumber)
+
+        currentNumber += 1
+
     return list
